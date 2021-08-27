b9c35ddff4d947e4b8d175342a7b6b5ce27f209e
Leoris <danielaaleoris@outlook.es>
2021-08-27 10:08:01 -0300

    schedule part 1

--- src/app/screens/schedule/schedule.module.ts
+++ src/app/screens/schedule/schedule.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
@@ -7,14 +7,28 @@ import { IonicModule } from '@ionic/angular';
 import { SchedulePageRoutingModule } from './schedule-routing.module';
 
 import { SchedulePage } from './schedule.page';
+import { NgCalendarModule } from 'ionic2-calendar';
+import { CalModalPageModule } from './../../pages/cal-modal/cal-modal.module';
+
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de'
+registerLocaleData(localeDe);
+
+
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    SchedulePageRoutingModule
+    SchedulePageRoutingModule,
+    NgCalendarModule,
+    CalModalPageModule
+    
   ],
-  declarations: [SchedulePage]
+  declarations: [SchedulePage],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'de-De'}
+  ]
 })
 export class SchedulePageModule {}
