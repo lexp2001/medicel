b2124803610f5f4f504f56f69a9d2571d423941e
Leoris <danielaaleoris@outlook.es>
2021-09-09 22:19:10 -0300

    changes home

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { AfterContentChecked, Component, ViewChild, ViewEncapsulation } from '@angular/core';
+import { from } from 'rxjs';
+import { SwiperOptions } from 'swiper';
+import { SwiperComponent } from 'swiper/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss']
+  styleUrls: ['home.page.scss'],
+  encapsulation: ViewEncapsulation.None
 })
-export class HomePage {
+export class HomePage implements AfterContentChecked{
+  @ViewChild('swiper') swiper: SwiperComponent;
 
+  config: SwiperOptions = {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    pagination: true
+  };
   constructor() {}
+
+  ngAfterContentChecked(){
+    if (this.swiper) {
+      this.swiper.updateSwiper({});
+    }
+  }
 
 }
