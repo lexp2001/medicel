7fa9cd334725717c6e6a4685c8b572018a993510
Leoris <danielaaleoris@outlook.es>
2021-09-13 06:29:47 -0300

    Google map error

--- src/app/events/events-details/events-details.page.ts
+++ src/app/events/events-details/events-details.page.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Geolocation } from '@ionic-native/geolocation/ngx';
+import {LoadingController} from '@ionic/angular';
+
+declare var google;
 
 @Component({
   selector: 'app-events-details',
@@ -8,13 +12,48 @@ import { Router } from '@angular/router';
 })
 export class EventsDetailsPage implements OnInit {
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private geolocation: Geolocation,
+    private loadingCtrl: LoadingController) { 
+     
+    }
+
 
   goHome(){
-    this.router.navigate(['/main/home'])
+    this.router.navigate(['/main/home']) 
   }
 
   ngOnInit() {
+    this.loadMap();
+  }
+
+  async loadMap(){
+    const loading = await this.loadingCtrl.create();
+    loading.present();
+    const rta = await this.geolocation.getCurrentPosition();
+    const myLatLng = {
+      lat: rta.coords.latitude,
+      lng: rta.coords.longitude
+    };
+    const mapEle: HTMLElement = document.getElementById('map');
+    const map = new google.maps.Map(mapEle, {
+      center: myLatLng,
+      zoom: 12
+    });
+    google.maps.EventsDetailsPage
+    .addListenerOnce(map, 'idle', () => {
+     loading.dismiss();
+     const marker = new google.maps.Marker({
+       position:{
+         lat: myLatLng.lat,
+         lng: myLatLng.lng
+       },
+       map: map,
+       title: 'Hello World'
+     });
+    });
+   
   }
 
 }
